b970057efb7dc1271e41062f0221efef54af91db
nedelyapyat <nedelyapyat@gmail.com>
2025-02-20 18:11:13 +0600

    Добавил классы для заданий

---
+++ 20.02.25_HomeTask_Egor_Mansur/20.02.25_HomeTask_Egor_Mansur/8_ArrayMaxValue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._02._25_HomeTask_Egor_Mansur
+{
+    internal class _8_ArrayMaxValue
+    {
+    }
+}
